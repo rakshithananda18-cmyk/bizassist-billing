5526071fc54632446bf36676ca988f3703fce296
bsalmagithub <salma.b.work@gmail.com>
2026-07-02 00:38:34 +0530

    Fix Postgres migration constraints rename handling

--- backend/alembic/versions/ac832002b0db_rename_tables_phase_3.py
+++ backend/alembic/versions/ac832002b0db_rename_tables_phase_3.py
@@ -43,35 +43,46 @@ def upgrade() -> None:
     # Postgres specific renames (indexes, constraints, policies)
     is_postgres = bind.dialect.name == 'postgresql'
     if is_postgres:
-        # 1. b2b_invite_codes
-        op.execute("ALTER INDEX ix_connection_codes_code RENAME TO ix_b2b_invite_codes_code")
-        op.execute("ALTER INDEX ix_connection_codes_id RENAME TO ix_b2b_invite_codes_id")
-        op.execute("ALTER INDEX ix_connection_codes_seller_code RENAME TO ix_b2b_invite_codes_seller_code")
-        op.execute("ALTER TABLE b2b_invite_codes RENAME CONSTRAINT pk_connection_codes TO pk_b2b_invite_codes")
-        op.execute("ALTER TABLE b2b_invite_codes RENAME CONSTRAINT fk_connection_codes_seller_business_id_users TO fk_b2b_invite_codes_seller_business_id_users")
-        op.execute("ALTER POLICY connection_codes_tenant_isolation ON b2b_invite_codes RENAME TO b2b_invite_codes_tenant_isolation")
+        safe_execute = """
+        DO $$
+        BEGIN
+            -- 1. b2b_invite_codes
+            BEGIN ALTER INDEX ix_connection_codes_code RENAME TO ix_b2b_invite_codes_code; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER INDEX ix_connection_codes_id RENAME TO ix_b2b_invite_codes_id; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER INDEX ix_connection_codes_seller_code RENAME TO ix_b2b_invite_codes_seller_code; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE b2b_invite_codes RENAME CONSTRAINT pk_connection_codes TO pk_b2b_invite_codes; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE b2b_invite_codes RENAME CONSTRAINT fk_connection_codes_seller_business_id_users TO fk_b2b_invite_codes_seller_business_id_users; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER POLICY connection_codes_tenant_isolation ON b2b_invite_codes RENAME TO b2b_invite_codes_tenant_isolation; EXCEPTION WHEN OTHERS THEN END;
 
-        # 2. b2b_ledgers
-        op.execute("ALTER INDEX ix_shared_ledgers_id RENAME TO ix_b2b_ledgers_id")
-        op.execute("ALTER INDEX ix_shared_ledgers_seller_buyer RENAME TO ix_b2b_ledgers_seller_buyer")
-        op.execute("ALTER TABLE b2b_ledgers RENAME CONSTRAINT pk_shared_ledgers TO pk_b2b_ledgers")
-        op.execute("ALTER TABLE b2b_ledgers RENAME CONSTRAINT fk_shared_ledgers_buyer_business_id_users TO fk_b2b_ledgers_buyer_business_id_users")
-        op.execute("ALTER TABLE b2b_ledgers RENAME CONSTRAINT fk_shared_ledgers_seller_business_id_users TO fk_b2b_ledgers_seller_business_id_users")
-        op.execute("ALTER POLICY shared_ledgers_tenant_isolation ON b2b_ledgers RENAME TO b2b_ledgers_tenant_isolation")
+            -- 2. b2b_ledgers
+            BEGIN ALTER INDEX ix_shared_ledgers_id RENAME TO ix_b2b_ledgers_id; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER INDEX ix_shared_ledgers_seller_buyer RENAME TO ix_b2b_ledgers_seller_buyer; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE b2b_ledgers RENAME CONSTRAINT pk_shared_ledgers TO pk_b2b_ledgers; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE b2b_ledgers RENAME CONSTRAINT fk_shared_ledgers_buyer_business_id_users TO fk_b2b_ledgers_buyer_business_id_users; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE b2b_ledgers RENAME CONSTRAINT fk_shared_ledgers_seller_business_id_users TO fk_b2b_ledgers_seller_business_id_users; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER POLICY shared_ledgers_tenant_isolation ON b2b_ledgers RENAME TO b2b_ledgers_tenant_isolation; EXCEPTION WHEN OTHERS THEN END;
 
-        # 3. action_logs
-        op.execute("ALTER INDEX ix_action_log_business_id RENAME TO ix_action_logs_business_id")
-        op.execute("ALTER INDEX ix_action_log_id RENAME TO ix_action_logs_id")
-        op.execute("ALTER TABLE action_logs RENAME CONSTRAINT pk_action_log TO pk_action_logs")
+            -- 3. action_logs
+            BEGIN ALTER INDEX ix_action_log_id RENAME TO ix_action_logs_id; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER INDEX ix_action_log_reference_id RENAME TO ix_action_logs_reference_id; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE action_logs RENAME CONSTRAINT pk_action_log TO pk_action_logs; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE action_logs RENAME CONSTRAINT fk_action_log_business_id_users TO fk_action_logs_business_id_users; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER POLICY action_log_tenant_isolation ON action_logs RENAME TO action_logs_tenant_isolation; EXCEPTION WHEN OTHERS THEN END;
 
-        # 4. ai_feedback
-        op.execute("ALTER INDEX ix_feedback_id RENAME TO ix_ai_feedback_id")
-        op.execute("ALTER TABLE ai_feedback RENAME CONSTRAINT pk_feedback TO pk_ai_feedback")
+            -- 4. ai_feedback
+            BEGIN ALTER INDEX ix_feedback_id RENAME TO ix_ai_feedback_id; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE ai_feedback RENAME CONSTRAINT pk_feedback TO pk_ai_feedback; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE ai_feedback RENAME CONSTRAINT fk_feedback_business_id_users TO fk_ai_feedback_business_id_users; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER POLICY feedback_tenant_isolation ON ai_feedback RENAME TO ai_feedback_tenant_isolation; EXCEPTION WHEN OTHERS THEN END;
 
-        # 5. ai_query_overrides
-        op.execute("ALTER INDEX ix_query_override_id RENAME TO ix_ai_query_overrides_id")
-        op.execute("ALTER TABLE ai_query_overrides RENAME CONSTRAINT pk_query_override TO pk_ai_query_overrides")
-        op.execute("ALTER TABLE ai_query_overrides RENAME CONSTRAINT uq_query_override_biz_query TO uq_ai_query_overrides_biz_query")
+            -- 5. ai_query_overrides
+            BEGIN ALTER INDEX ix_query_override_id RENAME TO ix_ai_query_overrides_id; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE ai_query_overrides RENAME CONSTRAINT pk_query_override TO pk_ai_query_overrides; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER TABLE ai_query_overrides RENAME CONSTRAINT fk_query_override_business_id_users TO fk_ai_query_overrides_business_id_users; EXCEPTION WHEN OTHERS THEN END;
+            BEGIN ALTER POLICY query_override_tenant_isolation ON ai_query_overrides RENAME TO ai_query_overrides_tenant_isolation; EXCEPTION WHEN OTHERS THEN END;
+        END $$;
+        """
+        op.execute(safe_execute)
 
 
 def downgrade() -> None:
